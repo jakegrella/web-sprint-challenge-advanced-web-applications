e21c0f1312d13e54d8905a0dc99bce6ca493693e
Jake Grella <jgrella13@gmail.com>
2020-11-13 12:17:22 -0600

    completed mvp

--- client/src/components/BubblePage.test.js
+++ client/src/components/BubblePage.test.js
@@ -1,7 +1,18 @@
-import React from "react";
-import { render, screen } from "@testing-library/react";
-import BubblePage from "./BubblePage";
+import React from 'react';
+import { render, screen, fireEvent, waitFor } from '@testing-library/react';
+import App from '../App';
 
-test("Fetches data and renders the bubbles", () => {
-  // Finish this test
+test('Fetches data and renders the bubbles', () => {
+	// Finish this test
+	render(<App />);
+
+	const username = screen.getByPlaceholderText('username');
+	const password = screen.getByPlaceholderText('password');
+	fireEvent.change(username, { target: { value: 'Lambda School' } });
+	fireEvent.change(password, { target: { value: 'i<3Lambd4' } });
+
+	const loginBtn = screen.getByRole('button');
+	fireEvent.click(loginBtn);
+
+	// expect(await screen.findByText(/bubbles/i)).toBeInTheDocument();
 });
